dbc62efe1517420b0a6486cfc7d8121ce449d1dc
Joel Frydman <joelfrydman@hotmail.com>
2023-05-18 17:49:41 -0300

    navigation bottom tab

--- src/navigation/ShopNavigator.jsx
+++ src/navigation/ShopNavigator.jsx
@@ -1,28 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native'
-
-import BreadDetailScreen from '../screens/BreadDetailScreen'
-import CategoriesScreen from '../screens/CategoriesScreen'
-import CategoryBreadScreen from '../screens/CategoryBreadScreen'
-import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { COLORS } from "../constants/colors";
+import CategoriesScreen from "../screens/CategoriesScreen";
+import DetailsScreen from "../screens/DetailsScreen";
+import ProductsScreen from "../screens/ProductsScreen";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const ShopNavigator = () => {
+  const Stack = createNativeStackNavigator();
 
-    const Stack = createNativeStackNavigator()
+  return (
+    <Stack.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.primary,
+        },
+        headerTintColor: COLORS.quaternary,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={CategoriesScreen}
+        options={{
+          title: "Categories",
+        }}
+      />
+      <Stack.Screen
+        name="Products"
+        component={ProductsScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+        })}
+      />
+      <Stack.Screen
+        name="Details"
+        component={DetailsScreen}
+        options={({ route }) => ({
+          title: route.params.name,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
 
-
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName='Home'>
-                <Stack.Screen name='Bread' component={CategoryBreadScreen} />
-                <Stack.Screen name='Home' component={CategoriesScreen} />
-                <Stack.Screen name='Detail' component={BreadDetailScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
-
-export default ShopNavigator
-
-const styles = StyleSheet.create({})
+export default ShopNavigator;
--- src/screens/CategoriesScreen.jsx
+++ src/screens/CategoriesScreen.jsx
@@ -1,17 +1,46 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, View } from "react-native";
 
-import React from 'react'
+import { CATEGORIES } from "../data/categories";
+import CategoriesItem from "../components/CategoriesItem";
+import React from "react";
 
 const CategoriesScreen = ({ navigation }) => {
-    return (
-        <View>
-            <Text>CategoriesScreen</Text>
-            <Button title='Navegar a Bread' onPress={() => navigation.navigate("Bread")} />
-        </View>
-    )
-}
+  const handleSelectedCategory = item => {
+    navigation.navigate("Products", {
+      categoryId: item.id,
+      name: item.title,
+    });
+  };
 
-export default CategoriesScreen
+  const renderCategoriesItem = ({ item }) => (
+    <View style={styles.categoriesContainer}>
+      <CategoriesItem item={item} onSelected={handleSelectedCategory} />
+    </View>
+  );
 
-const styles = StyleSheet.create({})
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={CATEGORIES}
+        renderItem={renderCategoriesItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  categoriesContainer: {
+    padding: 15,
+    height: 150,
+  },
+});
 
