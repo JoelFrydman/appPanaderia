aaa1d85d4d19a52166367e65c7dcceab5a93ab78
Joel Frydman <joelfrydman@hotmail.com>
2023-05-18 18:16:01 -0300

    Modificacion en app.js para un correcto renderizado

--- App.js
+++ App.js
@@ -1,52 +1,10 @@
-import { Dimensions, StyleSheet, Text, View } from 'react-native';
-import { useEffect, useState } from 'react';
-
-import ShopNavigator from './src/navigation/ShopNavigator';
-import { StatusBar } from 'expo-status-bar';
-import { useFonts } from 'expo-font';
+import BottomTabNavigator from "./src/navigation/BottomTabNavigator";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  const [isPortrait, setIsPortrait] = useState(true)
-
-  const statePortrait = () => setIsPortrait(onPortrait)
-
-  const onPortrait = () => {
-    const dim = Dimensions.get("screen")
-    return dim.height > dim.width
-  }
-
-  console.log(isPortrait);
-
-  useEffect(() => {
-    Dimensions.addEventListener('change', statePortrait)
-    return () => {
-      Dimensions.removeEventListener('change', statePortrait)
-    }
-  }, [])
-
-  const [loaded] = useFonts({
-    OpenSansBold: require('./src/assets/fonts/OpenSans-Bold.ttf'),
-    OpenSansRegular: require('./src/assets/fonts/OpenSans-Regular.ttf'),
-  });
-
-  if (!loaded) return null
-
   return (
-      <ShopNavigator />
+    <NavigationContainer>
+      <BottomTabNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  texto: {
-    fontFamily: "OpenSansBold"
-  },
-  texto2: {
-    fontFamily: "OpenSansRegular"
-  }
-});
